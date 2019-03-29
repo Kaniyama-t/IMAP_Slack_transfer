48df08d12e037ea9b81dfe8b6b39a308ad582ef5
Kaniyama_t <webmaster@kaniyama.net>
2019-03-30 06:23:29 +0900

    changed file name
    imap_root.py > imap_observer.py
    
    slackapi.py > imap_slackapi.py

---
+++ imap_observer.py
@@ -0,0 +1,89 @@
+
+from imap_module_wrapper import IMAPConnection
+from imap_slackapi import SlackPoster
+import requests
+import configparser
+import os.path
+import sys
+
+
+# = 設定ファイル ロード ========================================================
+envConfig = configparser.ConfigParser()
+envConfig.read('../slacknotify_envsettings.ini')
+addressList = configparser.ConfigParser()
+addressList.read('slacknotify_addresslist.ini')
+poricyList = configparser.ConfigParser()
+poricyList.read('slacknotify_domainlist.ini')
+
+# = 新規メールのチェック・Slack転送 =============================================
+## 核心的処理
+def mailCheckAndProcess(session, mailBoxName):
+    ## Flagがついてないメールの番号を取得
+    session.selectBox(mailBoxName)
+    mails=session.getNoFlaggedMail()
+    ## 各メールについて処理
+    
+    for i in mails[0].split():
+        ## メールを取得
+        mail=session.getMailDetail(i)
+        mailaddressInfo = addressList.items(section=section)
+        ## メールアドレスと紐づいた全ての受付チャンネルについて処理
+        ProceedCnt = 0
+        FinishedResponces = []
+        for p in mailaddressInfo:
+            if('imap_notify' in p[0]):
+                ## 受信チャンネルへメール転送
+                ProceedCnt = ProceedCnt + 1
+                try:
+                    slackpt=SlackPoster()
+                    responce=slackpt.IMAPMailPost(
+                        channelId = p[1],
+                        uid = mail['UID'],
+                        succeedState = mail['Succeed'],
+                        mailBoxName = mailBoxName,
+                        mailFromAdd = mail['From_Address'],
+                        mailFromName = mail['From_Name'],
+                        mailToAdd = mail['To_Address'],
+                        mailToName = mail['To_Name'],
+                        mailsub = mail['Subject'],
+                        mailbody = mail['Body']
+                    )
+                    session.addFlagToMail(i,'Slack_'+responce['ts'])
+                    FinishedResponces.append(responce)
+                except Exception as Err_inst:
+                    print(Err_inst)
+                    continue
+        if ProceedCnt == len(FinishedResponces):
+            print('Transfar Mail Completed of mail '+str(i))
+            session.addFlagToMail(i,'POSTED_SLACK_COMPLETED')
+        if len(FinishedResponces) != 0:
+            print('Transfar Mail of '+str(i))
+            print(' - RequirePost:'+str(ProceedCnt))
+            print(' - CompletedPost:'+str(len(FinishedResponces)))
+            session.addFlagToMail(i,'POSTED_SLACK')
+        session.commitFrags()
+        print('commited flag')
+    session.closeBox()
+    
+
+## めんどい色々
+for sections in addressList.items():
+    # [ここから] メールアドレス毎に実行
+    section = sections[0]
+    if section == "DEFAULT":
+        continue
+    # IMAPサーバURL/Port ロード
+    poricyName = addressList[section]['poricy']
+    IMAPserverUrl = poricyList[poricyName]['imap_address']
+    IMAPServerPort = poricyList[poricyName]['imap_port']
+    # IMAPサーバ接続
+    session = IMAPConnection(IMAPserverUrl,IMAPServerPort) # Sock Session作成
+    session.login(addressList[section]['imap_user'],addressList[section]['imap_pass']) # Login
+    ## 全メールのチェック・転送・マーク処理
+    mailCheckAndProcess(session,'INBOX')
+    mailCheckAndProcess(session,'INBOX.Sent')
+    ## 後始末
+    session.logout()
+    # [ここまで] メールアドレス毎に実行
+
+
---
+++ imap_slackapi.py
@@ -0,0 +1,119 @@
+import configparser
+import requests
+import imap_errors
+import json
+
+class SlackPoster():
+
+    def __init__(self):
+        # Hueee
+        config = configparser.ConfigParser()
+        config.read('../slacknotify_envsettings.ini')
+        self.config = config
+
+    def post(self, channelId, iconURL=None, username=None, ts=None, text=None, attachments=None):
+        token = self.config['slack_api']['token']
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+token
+            }
+        req = {
+            "channel":channelId,
+            "token": token
+            }
+        if iconURL != None:
+            req["icon_url"] = iconURL
+        if username != None:
+            req["username"] = username
+        if ts != None:
+            req["thread_ts"] = ts
+        if text != None:
+            req["text"] = text
+        if attachments != None:
+            req["attachments"] = attachments
+        ressrc = requests.post("https://slack.com/api/chat.postMessage", data=json.dumps(req), headers=headers)
+        res = ressrc.json()
+        print('[IMAP_transfar_SlackPost] Query sended to chat.postMessage')
+        print('                           State:'+str(res['ok']))
+        print('                           Channel:'+res['channel'])
+        print('                           ts:'+res['ts'])
+        #print('                           message:'+str(res['message']))
+        print('')
+        if res['ok'] != True:
+            raise imap_errors.SlackPostError(res)
+        
+        return res
+
+    def defPost(self, channelId, text, ts=None):
+        # わーいポストします(ぽーん)
+        self.post(channelId=channelId, text=text, ts=ts)
+
+    def IMAPMailPost(self, channelId, uid, succeedState, mailBoxName, mailFromAdd, mailFromName, mailToAdd, mailToName, mailsub, mailbody, msg=None, ts=None):
+        ############## genelate common postBody
+        postBody = [
+        {
+            "fallback": mailbody,
+            "color": '',
+            "author_name": '',
+            "author_link": '',
+            #"author_icon": "https://pbs.twimg.com/profile_images/1068841466988920832/wMIpsqCY.jpg",
+            "title": mailsub+"#"+str(uid),
+            "title_link": "",
+            "text": mailbody,
+            "actions": [
+                {
+                    "name": "btn2Name",
+                    "text": "別チャンネルへ転送",
+                    "type": "button",
+                    "style": "primary",
+                    "value": "shareValue"
+                }
+            ],
+            "footer": ''#,
+            #"footer_icon": "https://pbs.twimg.com/profile_images/1068841466988920832/wMIpsqCY.jpg"
+        }]
+        # リソース作成
+        mailFrom_display = mailFromAdd
+        mailTo_display = mailToAdd
+        if mailFromName != '':
+            mailFrom_display = mailFromName+" ("+mailFromAdd+")"
+        if mailToName != '':
+            mailTo_display = mailToName+" <"+mailToAdd+">"
+        # PostMessage時オプションの初期化
+        PostUserIcon = ''
+        PostUserName = 'メール転送'
+        PostStateText = ''
+
+        ###### Edit Body
+        # 正しく取得(sock通信/decode/encode)できたデータか判定(以下判定の後)
+        # 受信メールなら
+        if mailBoxName == 'INBOX':
+            # TODO ここに
+            PostStateText = '<@channel> `メールを受信しました'
+            PostUserIcon = 'https://takuma-isec.sakura.ne.jp/kaniyama_t/slackapp/src/imaptrans_received.png'
+            postBody[0]['color'] = "#36a64f"
+            postBody[0]['author_name'] = mailFrom_display
+            postBody[0]['author_link'] = 'mailto:'+mailFromAdd
+            postBody[0]['footer'] = "受取: " + mailTo_display
+            postBody[0]['actions'].append({
+                    "name": "btn1Name",
+                    "text": "返信",
+                    "type": "button",
+                    "style": "default",
+                    "value": "replyValue"
+                })
+        elif mailBoxName == 'INBOX.Sent':
+            PostStateText = '<@channel> `メールの送信を検知しました'
+            PostUserIcon = 'https://takuma-isec.sakura.ne.jp/kaniyama_t/slackapp/src/imaptrans_sended.png'
+            postBody[0]['color'] = "#ffff00"
+            postBody[0]['author_name'] = mailTo_display
+            postBody[0]['author_link'] = 'mailto:'+mailToAdd
+            postBody[0]['footer'] = "送信: " + mailFrom_display
+
+        if succeedState != True:
+            PostStateText = PostStateText+"が、正常にメールを読み込めませんでした"
+            postBody[0]['color'] = "#ff0000"
+            
+        
+        return self.post(channelId=channelId,ts=ts, text=PostStateText+'`\n', attachments=postBody,username=PostUserName,iconURL=PostUserIcon)
+        
